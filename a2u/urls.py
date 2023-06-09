03e9c7113fec26ebc1b7fb9c0cee96ee44009ae8
RYU JAE YEONG <fbwodud4943@naver.com>
2023-06-09 12:08:22 +0900

    업로드 방식개선

--- a2u/urls.py
+++ a2u/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('image/', views.image, name='image')
+    path('image/', views.image, name='image'),
+    path('upload/', views.upload_file, name='upload_file'),
+
 ]
--- a2u/views.py
+++ a2u/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .test_image import load_image, test
 from django.core.files.storage import default_storage
 
@@ -30,3 +30,14 @@ def image(request):
     test(dict_to_class(args, "args"))
     return HttpResponse(file_name.name)
 
+def upload_file(request):
+    if request.method == 'POST':
+        file = request.FILES.get('file')
+        # 파일 처리 로직을 여기에 작성하세요
+
+        # 예시: 파일 처리 후, 파일 크기와 파일 URL을 반환합니다.
+        file_size = file.size
+        file_url = "https://example.com/uploads/" + file.name
+        return JsonResponse({'file_size': file_size, 'file_url': file_url})
+    else:
+        return render(request, 'zone.html')  # 파일 업로드 폼 템플릿을 반환합니다.
